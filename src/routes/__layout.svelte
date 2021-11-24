71c54bd75b5dfc1f52afdafc885a66bd018058cf
Hunter Perrin <hperrin@gmail.com>
2021-11-23 17:06:22 -0800

    Update to latest SMUI. Add Top App Bar and links.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-	import Button, { Label } from '@smui/button';
+	import Button from '@smui/button';
+	import TopAppBar, {
+		Row,
+		Section,
+		Title,
+		AutoAdjust,
+		TopAppBarComponentDev
+	} from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+	import { Label, Icon } from '@smui/common';
+	import { Svg } from '@smui/common/elements';
+	import { mdiGithub, mdiWeb } from '@mdi/js';
+
+	let topAppBar: TopAppBarComponentDev;
 
 	let lightTheme =
 		typeof window === 'undefined' || window.matchMedia('(prefers-color-scheme: light)').matches;
@@ -18,11 +31,31 @@
 	}
 </script>
 
-<div style="display: flex; justify-content: space-between;">
+<TopAppBar bind:this={topAppBar} variant="standard">
+	<Row>
+		<Section>
+			<Title>My App</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton aria-label="GitHub" href="https://github.com/hperrin/svelte-material-ui">
+				<Icon component={Svg} viewBox="0 0 24 24">
+					<path fill="currentColor" d={mdiGithub} />
+				</Icon>
+			</IconButton>
+			<IconButton aria-label="Demo Site" href="https://sveltematerialui.com">
+				<Icon component={Svg} viewBox="0 0 24 24">
+					<path fill="currentColor" d={mdiWeb} />
+				</Icon>
+			</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<AutoAdjust {topAppBar} style="display: flex; justify-content: space-between;">
 	<div class="container"><slot /></div>
 	<div class="container">
 		<Button on:click={switchTheme}>
 			<Label>{lightTheme ? 'Lights off' : 'Lights on'}</Label>
 		</Button>
 	</div>
-</div>
+</AutoAdjust>
